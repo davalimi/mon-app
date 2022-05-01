1450c3778374b5efc894393915744ca2d02a3591
David <30422759+davalimi@users.noreply.github.com>
2022-05-01 14:35:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ceci est un test de ouffff !
         </a>
       </header>
     </div>
